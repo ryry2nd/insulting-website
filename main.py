ee89cfe5e1788c55a3aa0170e943f324cd77b791
Ryan Stouffer2 <ryry2nd@gmail.com>
2022-06-17 14:52:21 -0400

    error cheacking and optimisation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, Request
 from waitress import serve
 from pysondb import PysonDB
-import socket, html
+import socket, html, pysondb
 
 #get ip
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -15,27 +15,28 @@ db = PysonDB("data.json")
 app = Flask(__name__)
 
 #init constants
+LENGTH_LIMIT = 100
 HOST = "0.0.0.0"
 THREADS = 6
-PORT = 8080
-
-#gets all of the names
-def getNames():
-	for id, value in db.get_all().items():
-		yield value["name"]+":"+str(id)
+PORT = 80
 
 #sets up the data part of the website
 @app.route("/data", methods=['POST', 'GET'])
 def names():
 	#if it is getting data, return all of the names
 	if request.method == 'GET':
-		return render_template("allData.html", names=getNames())
+		return render_template("allData.html", data = db.get_all().items())
 	#if it is sending data, give them the data
 	else:
-		id = html.escape(request.form["id"])
-		data = db.get_by_id(id)
-
-		return render_template("name.html", name=data["name"], id=id, pro=data["pro"], ssn=data["ssn"])
+		try:
+			id = html.escape(request.form["id"])
+			if id == "":
+				return render_template("error.html", error="bruh you can't make it blank")
+			else:
+				data = db.get_by_id(id)
+				return render_template("name.html", name=data["name"], id=id, pro=data["pro"], ssn=data["ssn"])
+		except pysondb.errors.IdDoesNotExistError:
+			return render_template("error.html", error=f"id: {id}, not found")
 
 #sets up the index part of the website
 @app.route("/", methods=['POST', 'GET'])
@@ -48,8 +49,13 @@ def index():
 		name = html.escape(request.form["name"])
 		pro = html.escape(request.form["pro"])
 		ssn = html.escape(request.form["ssn"])
-		db.add({"name": name, "pro": pro, "ssn": ssn})
-		return render_template("greeting.html", name=name, pro=pro, ssn=ssn)
+		if name == "" or pro == "" or ssn == "":
+			return render_template("error.html", error="bruh you can't make it blank")
+		elif len(name)>LENGTH_LIMIT or len(pro)>LENGTH_LIMIT or len(ssn)>LENGTH_LIMIT:
+			return render_template("error.html", error="bruh, that's way too long")
+		else:
+			db.add({"name": name, "pro": pro, "ssn": ssn})
+			return render_template("greeting.html", name=name, pro=pro, ssn=ssn)
 
 #if it is not being imported print the name and ip address and start up the website
 if __name__ == '__main__':
